f79f540fb78f69d7549acc746eea6552a49c9987
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-05-24 16:40:22 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR="/mnt/unie-os-x86"
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
@@ -32,8 +33,15 @@ else
   echo -e  "${RED}[E]${RESET}: This environment is not a Linux distribution! Exiting"
   exit 1
 fi
-
-command -v "git" >/dev/null && echo -e "${BLUE}[I]${RESET}: git is installed. Continue" || sudo apt install git
-command -v "curl" >/dev/null && echo -e  "${BLUE}[I]${RESET}: curl is installed. Continue" || sudo apt install curl
-command -v "make" >/dev/null && echo -e "${BLUE}[I]${RESET}: make is installed. Continue" || sudo apt install make
+sudo mkdir -p "$DIR"
+sudo mkdir -p "$DIR/usr"
+sudo mkdir -p "$DIR/usr/local"
+sudo mkdir -p "$DIR/usr/local/ucomp"
+sudo mkdir -p "$DIR/usr/bin"
+UCOMPDIR="$DIR/usr/local/ucomp"
+command -v "git" >/dev/null && echo -e "${BLUE}[I]${RESET}: git is installed. Continue" || sudo apt install -y git
+command -v "curl" >/dev/null && echo -e  "${BLUE}[I]${RESET}: curl is installed. Continue" || sudo apt install -y curl
+command -v "make" >/dev/null && echo -e "${BLUE}[I]${RESET}: make is installed. Continue" || sudo apt install -y make
+sudo curl -L https://raw.githubusercontent.com/UnieOS/UnieOS/refs/heads/unie-0.1.0/usr.bin/utop.c -o "$DIR/usr/local/ucomp/utop.c"
+sudo clang "$UCOMPDIR/utop.c" -v -o "$DIR/usr/bin/utop"
 exit 0 # Script on development. Wait
