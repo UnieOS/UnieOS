fc5256afa8fa0e4787adf10668e6c059d0c19d65
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-06-01 17:34:34 -0300

    Add string.h header file with basic string functions

---
+++ unielibc/include/string.h
@@ -0,0 +1,13 @@
+#ifndef MYLIBC_STRING_H
+#define MYLIBC_STRING_H
+// written by llms
+// caution
+
+typedef unsigned long size_t;
+
+size_t strlen(const char *s);
+void *memcpy(void *dest, const void *src, size_t n);
+long write(int fd, const void *buf, size_t count);
+
+#endif
+
