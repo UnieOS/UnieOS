1249e8a5b5889872cf0206553d12f7c4db21b161
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-06-03 23:57:31 -0300

    Add error message for UnieOS installation failure

---
+++ randomstuff.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo.
+echo ==============================
+echo   UnieOS INSTALL FAILED
+echo ==============================
+echo.
+echo Only Linux environments supported
+echo Sorry :(
+echo.
+pause
