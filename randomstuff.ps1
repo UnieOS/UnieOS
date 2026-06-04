1eb18430c04ed7da2e8e651b552ef50725ce21d9
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-06-04 13:07:37 -0300

    Add error message for UnieOS installation failure

---
+++ randomstuff.ps1
@@ -0,0 +1,18 @@
+Clear-Host
+
+Write-Output ""
+Write-Output "=============================="
+Write-Output "  UnieOS install failed"
+Write-Output "=============================="
+Write-Output ""
+Write-Output "Only Linux environments supported"
+Write-Output "Sorry :("
+Write-Output ""
+Write-Output "You can see supported Linux environments here:"
+Write-Output "https://www.ubuntu.com"
+Write-Output "https://linuxmint.com"
+Write-Output "https://debian.org"
+Write-Output "https://devuan.org"
+Write-Output ""
+
+Read-Host
