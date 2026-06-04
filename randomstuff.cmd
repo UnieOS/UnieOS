1ab5b241ba7844fce50a2b0c96eee60ef9c22786
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-06-03 23:59:33 -0300

    Improve UnieOS installation failure message
    Updated error message and added links to supported Linux environments.

--- randomstuff.cmd
+++ randomstuff.cmd
@@ -1,10 +1,16 @@
 @echo off
 echo.
 echo ==============================
-echo   UnieOS INSTALL FAILED
+echo   UnieOS install failed
 echo ==============================
 echo.
 echo Only Linux environments supported
 echo Sorry :(
+echo.
+echo You can see supported Linux environments here:
+echo https://www.ubuntu.com
+echo https://linuxmint.com
+echo https://debian.org
+echo https://devuan.org
 echo.
 pause
