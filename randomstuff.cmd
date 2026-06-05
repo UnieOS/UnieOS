f90a08f85bde573ffa0e527514e37cc809a0e824
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-06-04 21:39:19 -0300

    Include OS version in UnieOS install failure message
    Added OS version information to error message.

--- randomstuff.cmd
+++ randomstuff.cmd
@@ -1,4 +1,6 @@
 @echo off
+for /f "delims=" %%i in ('ver') do set VAR=%%i
+
 echo.
 echo ==============================
 echo   UnieOS install failed
@@ -7,6 +9,7 @@ echo.
 echo Only Linux environments supported
 echo Sorry :(
 echo.
+echo You are using %VAR%
 echo You can see supported Linux environments here:
 echo https://www.ubuntu.com
 echo https://linuxmint.com
